d76c081d80cf6a1215f65de481e2c8f30f7c748c
kyungmin <kyungmin727@gmail.com>
2024-04-12 10:48:41 +0900

    Feat:MagazineListItem a태그 link태그로 수정

--- src/pages/magazine/MagazineListItem.jsx
+++ src/pages/magazine/MagazineListItem.jsx
@@ -1,15 +1,17 @@
+import { Link } from 'react-router-dom';
+
 function MagazineListItem() {
   return (
     <>
       <li>
-        <a className="card" href="/magazine/detail">
+        <Link className="card" href="/magazine/detail">
           <img className="card-cover" src="public/cafe_coeo.jpeg" alt="" />
           <div className="card-contents">
             <span className="card-type yellow">USEFUL</span>
             <h4 className="card-title mini">Achim 스폿 서른 네 번째 파트너, 코에오 커피</h4>
           </div>
           <p className="card-footer">1일전 조회 72</p>
-        </a>
+        </Link>
       </li>
     </>
   );
