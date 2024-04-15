69834c3253ead274240444776c009590ec3b342d
sulahhong <100662448+sulahhong@users.noreply.github.com>
2024-04-15 10:51:15 +0900

    ✨ Feat: zustand/store에 products, cartProducts 추가

--- src/zustand/store.js
+++ src/zustand/store.js
@@ -3,16 +3,36 @@ import { persist } from 'zustand/middleware';
 
 const useUserStore = create(
   persist(
-    (set) => ({
+    (set, get) => ({
+      products: [],
+      newProducts: [],
+      cartProducts: [],
       token: null,
+      user: null,
+      setProducts: (data) => {
+        set({ products: data });
+      },
+      setNewProducts: (data) => {
+        if (data?.length > 0) {
+          let newData = data.filter((item) => item.extra?.isNew).slice(0, 4);
+          set({ newProducts: newData });
+        }
+      },
+      setCartProducts: (data) => {
+        console.log('CART', data);
+        if (data.item) {
+          const filteredData = data.item.filter((item) => item.quantity > 0);
+          set({ cartProducts: filteredData });
+        }
+      },
       setUserId: (token) => set({ token }),
-      user: null, // 새로운 상태 추가
-      setUser: (user) => set({ user }), // setUser 액션 추가
+      setUser: (user) => set({ user }),
     }),
     {
-      name: 'Acctoken',
+      name: 'Acctoken', // Persist configuration for localStorage
     },
   ),
 );
 
 export default useUserStore;
+
