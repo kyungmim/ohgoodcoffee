46e3a4c78d4fab155aee2350d835a9129879d076
kyungmin <kyungmin727@gmail.com>
2024-04-21 15:33:18 +0900

    ✨ Feat: 로딩페이지 파일 생성 및 적용 메인페이지 스와이퍼 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import { Suspense } from 'react';
 import router from '@/routes';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import Loading from '@/Loading';
 
 // react-query 사용
 const queryClient = new QueryClient();
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <Suspense>
+        <Suspense fallback={<Loading />}>
           <RouterProvider router={router} />
         </Suspense>
       </QueryClientProvider>
