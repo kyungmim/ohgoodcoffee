9a2c24bfbc33f1160305139e517e7c1a029a1b72
kyungmin <kyungmin727@gmail.com>
2024-04-23 10:58:48 +0900

    💄 Style: 마켓 매거진 마이페이지 ui반응형 작업

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,47 @@
 function About() {
   return (
-    <div>
-      <h2>About 페이지 입니다~</h2>
+    <div className="l_wrapper">
+      <h2 className="about-title">보람3조가 최고임.. 아무튼 체고~</h2>
+      <div className="about">
+        <div className="about-cover"></div>
+        <div className="about-contents">
+          <div className="about-text">
+            <p className="about-descripton">
+              <pre>
+                .∧,,∧ (；ω；) 하얗게
+                <br />
+                .∪　∪
+                <br />
+                し‐Ｊ
+                <br />
+                ∧ ,';,';,',.
+                <br />
+                ( ; ω,';,',. 불태웠어..
+                <br />
+                .∪　∪
+                <br />
+                し‐Ｊ
+                <br />
+                　　,';,',.
+                <br />
+                　';',,'...
+                <br />
+                .∪';',,'...
+                <br />
+                し‐Ｊ
+                <br />
+                <br />
+                　　';,';,',.
+                <br />
+                　,';,',',..
+                <br />
+                ';',,'...
+                <br />
+              </pre>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
